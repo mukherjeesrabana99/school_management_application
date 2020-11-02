4cd3f8e50c4c0083f835a26901b333ffedf63f27
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2020-11-02 15:42:28 +0530

    Add files via upload

--- students/views.py
+++ students/views.py
@@ -5,6 +5,7 @@ from .models import Student
 from .forms import StudentForm
 
 # Create your views here.
+#admin can view student list
 def student_list(request):
 	students=Student.objects.all().filter(status=True)
 	paginator = Paginator(students, 5)
@@ -12,6 +13,7 @@ def student_list(request):
 	page_obj=paginator.get_page(page_number)
 	context={'students':page_obj}
 	return render(request, 'students/student_list.html', context)
+#admin can add student
 def create_student(request):
 	form=StudentForm()
 	if request.method=='POST':
@@ -22,10 +24,12 @@ def create_student(request):
 	else:
 		context={'form':form}
 		return render(request, 'students/create_student.html', context)
+#admin can view student ptofile
 def student_profile(request, id):
 	student=Student.objects.get(id=id)
 	context={'student':student}
 	return render(request, 'students/student_profile.html', context)
+#admin can edit student
 def edit_student(request, id):
 	student=Student.objects.get(id=id)
 	form=StudentForm(instance=student)
@@ -37,20 +41,23 @@ def edit_student(request, id):
 	else:
 		context={'form':form}
 		return render(request, 'students/edit_student.html', context)
+#Admin can delete student
 def delete_student(request, id):
 	student=Student.objects.get(id=id)
 	student.delete()
 	return redirect('student_list')
-
+#admin can view unapproved student list
 def unapproved_student_list(request):
 	students=Student.objects.all().filter(status=False)
 	context={'students':students}
 	return render(request, 'students/unapproved_student_list.html', context)
+#admin can approve an unapproved student
 def approve_student(request, id):
 	student=Student.objects.get(id=id)
 	student.status=True
 	student.save()
 	return redirect('unapproved_student_list')
+#admin can cancel the student without approving him
 def cancel_student(request,id):
 	student=Student.objects.get(id=id)
 	student.delete()
