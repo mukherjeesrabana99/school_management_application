0e5f54831df0559a32d0139889b34a2fe0538b23
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2020-11-02 15:45:30 +0530

    Add files via upload

--- timetable/views.py
+++ timetable/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Timetable
 from .forms import TableForm
 # Create your views here.
+#Admin can view timetable list of all classes
 def timetable_list(request):
 	tables=Timetable.objects.all()
 	context={'tables':tables}
 	return render(request, 'timetable/table_list.html', context)
+#Admin can create Time table
 def create_timetable(request):
 	form=TableForm()
 	if request.method=='POST':
@@ -16,6 +18,7 @@ def create_timetable(request):
 	else:
 		context={'form':form}
 		return render(request, 'timetable/create_timetable.html', context)
+#Admin can edit timetable
 def edit_timetable(request, id):
 	table=Timetable.objects.get(id=id)
 	form=TableForm(instance=table)
@@ -27,6 +30,7 @@ def edit_timetable(request, id):
 	else:
 		context={'form':form}
 		return render(request, 'timetable/edit_timetable.html', context)
+#Admin can delete timetable
 def delete_timetable(request, id):
 	table=Timetable.objects.get(id=id)
 	table.delete()
