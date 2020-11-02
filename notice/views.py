a364ffeb4bd2e1f92696aabdad8cbfd0257fc96b
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2020-11-02 15:05:27 +0530

    Add files via upload

--- notice/views.py
+++ notice/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Notice
 from .forms import NoticeForm
 # Create your views here.
+#Admin can view notice
 def notice_list(request):
 	notices=Notice.objects.all()
 	context={'notices':notices}
 	return render(request, 'notice/notice_list.html', context)
-
+#Admin can create notice
 def create_notice(request):
 	form=NoticeForm()
 	if request.method=='POST':
@@ -18,7 +19,7 @@ def create_notice(request):
 		
 		context={'form':form}
 		return render(request, 'notice/create_notice.html', context)
-
+#Admin can edit notice
 def edit_notice(request, id):
 	notice=Notice.objects.get(id=id)
 	form=NoticeForm(instance=notice)
@@ -30,6 +31,7 @@ def edit_notice(request, id):
 	else:
 		context={'form':form}
 		return render(request, 'notice/edit_notice.html', context)
+#Admin can delete notice
 def delete_notice(request, id):
 	notice=Notice.objects.get(id=id)
 	notice.delete()
