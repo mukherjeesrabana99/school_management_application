0ea7c113f0802125067a284996c4db14dd9dacb1
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2020-11-02 16:05:18 +0530

    Add files via upload

--- academics/views.py
+++ academics/views.py
@@ -1,63 +1,178 @@
 from django.shortcuts import render, redirect
-from academics.models import Session, SchoolClass, Section, Subject, Shift, RegisteredClass
-from .models import School, Fees
-from faculty.models import Faculty
-from students.models import Student
-from notice.models import Notice
-from .forms import SchoolForm, FeesForm
-from events.models import Event, EventMember
-from students.models import Student
-from online_tests.models import Exam, ExamMember
+from django.core.paginator import Paginator
+from .models import SchoolClass, Section, Session, Shift,  RegisteredClass, Subject
+from.forms import SchoolClassForm, SectionForm, SessionForm,  ShiftForm, RegisteredClassForm, SubjectForm
+from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
 # Create your views here.
-#Admin can view class count, shift count, section count, current session,schoolname, subject count,
-#faculty count, student count, event count, notice count, exam counts
-
-def index(request):
-	class_count=SchoolClass.objects.all().count()
-	section_count=Section.objects.all().count()
-	shift_count=Shift.objects.all().count()
-	subject_count=Subject.objects.all().count()
-	regcl_count=RegisteredClass.objects.all().count()		
-	session=Session.objects.get(current=True)
-	notices=Notice.objects.all().order_by('-id')
-	notice_count=notices.count()
-	faculty=Faculty.objects.all().filter(status=True)
-	faculty_count= faculty.count()
-	students=Student.objects.all().filter(status=True)
-	student_count=students.count()
-	event_count=Event.objects.all().count()
-	member_count=EventMember.objects.all().count()
-	school=School.objects.get(current=True)
-	scheduled_exam_count=Exam.objects.all().filter(status="Scheduled").count()
-	completed_exam_count=Exam.objects.all().filter(status="Completed").count()
-	ongoing_exam_count=Exam.objects.all().filter(status="Ongoing").count()
-
-	context={'session':session, 'notices':notices, 'notice_count':notice_count,   'school':school, 
-	'faculty_count':faculty_count, 'student_count':student_count, 'class_count':class_count,
-	'shift_count':shift_count, 'subject_count':subject_count, 'section_count':section_count,
-	'regcl_count':regcl_count, 'event_count':event_count, 'member_count':member_count,
-	'scheduled_exam_count':scheduled_exam_count, 'completed_exam_count':completed_exam_count ,
-	 'ongoing_exam_count':ongoing_exam_count}
-	return render(request, 'administration/index.html', context)
-#Crendering fees page, to view and construct  fees structure
-def fees_page(request):
-	form=FeesForm()
+# Admin can create class
+def create_class(request):
+	class_form=SchoolClassForm()
 	if request.method=='POST':
-		form=FeesForm(request.POST)
+		form=SchoolClassForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect('fees_page')
+			return redirect('create_class')
 	else:
 		classes=SchoolClass.objects.all()
-		fees=Fees.objects.all()
-		context={'form':form, 'classes':classes, 'fees':fees }
-		return render(request, 'administration/fees.html', context)
-	
-#view fee payment status of each student classwisw
-def fee_detail(request, cl):
-	students=Student.objects.all().filter(class_name=cl)
-	context={'students':students}
-	return render(request, 'administration/fee_detail.html', context)
-from django.shortcuts import render
-
-
+		context={'form':class_form, "classes":classes}
+		return render(request, 'academics/class.html', context)
+#Admin edit class
+def edit_class(request, cl_id):
+	cl=SchoolClass.objects.get(id=cl_id)
+	form=SchoolClassForm(instance=cl)
+	if request.method=='POST':
+		form=SchoolClassForm(request.POST, instance=cl)
+		if form.is_valid():
+			form.save()
+			return redirect('create_class')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_class.html', context)
+#admin can delete class
+def delete_class(request, cl_id):
+	cl=SchoolClass.objects.get(id=cl_id)
+	cl.delete()
+	return redirect('create_class')
+# Admin can create section
+def create_section(request):
+	section_form=SectionForm()
+	if request.method=='POST':
+		form=SectionForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('create_section')
+	else:
+		sections=Section.objects.all()
+		context={'form':section_form, 'sections':sections}
+		return render(request, 'academics/section.html', context)
+#Admin can edit section
+def edit_section(request, sec_id):
+	sec=Section.objects.get(id=sec_id)
+	form=SectionForm(instance=sec)
+	if request.method=='POST':
+		form=SectionForm(request.POST, instance=sec)
+		if form.is_valid():
+			form.save()
+			return redirect('create_section')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_section.html', context)
+#Admin can delete section
+def delete_section(request, sec_id):
+	sec=Section.objects.get(id=sec_id)
+	sec.delete()
+	return redirect('create_section')
+# Admin can create session
+def create_session(request):
+	form=SessionForm()
+	if request.method=='POST':
+		form=SessionForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('create_session')
+	else:
+		sessions=Session.objects.all()
+		context={'form':form, 'sessions':sessions}
+		return render(request, 'academics/session.html', context)
+#Admin can edit session
+def edit_session(request, ses_id):
+	ses=Session.objects.get(id=ses_id)
+	form=SessionForm(instance=ses)
+	if request.method=='POST':
+		form=SessionForm(request.POST, instance=ses)
+		if form.is_valid():
+			form.save()
+			return redirect('create_session')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_session.html', context)
+#admin can delete session
+def delete_session(request, ses_id):
+	ses=Session.objects.get(id=ses_id)
+	ses.delete()
+	return redirect('create_session')
+# Admin create shift
+def create_shift(request):
+	form=ShiftForm()
+	if request.method=='POST':
+		form=ShiftForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('create_shift')
+	else:
+		shifts=Shift.objects.all()
+		context={'form':form, 'shifts':shifts}
+		return render(request, 'academics/shift.html', context)
+#admin can edit shift
+def edit_shift(request, shift_id):
+	shift=Shift.objects.get(id=shift_id)
+	form=ShiftForm(instance=shift)
+	if request.method=='POST':
+		form=ShiftForm(request.POST, instance=shift)
+		if form.is_valid():
+			form.save()
+			return redirect('create_shift')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_shift.html', context)
+#Admin can delete shift
+def delete_shift(request, shift_id):
+	shift=Shift.objects.get(id=shift_id)
+	shift.delete()
+	return redirect('create_shift')
+# Create registered class using  Registered Class Model
+def create_registeredclass(request):
+	form=RegisteredClassForm()
+	if request.method=='POST':
+		form=RegisteredClassForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('create_registeredclass')
+	else:
+		classes=RegisteredClass.objects.all()
+		context={'form':form, 'classes':classes}
+		return render(request, 'academics/reg_class.html', context)
+# Applying CRUD to Subject Model
+def create_subject(request):
+	form=SubjectForm()
+	if request.method=='POST':
+		form=SubjectForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('create_subject')
+	else:
+		subjects=Subject.objects.all()
+		context={'form':form, 'subjects':subjects}
+		return render(request, 'academics/subject.html', context)
+def edit_subject(request, sub_id):
+	sub=Subject.objects.get(id=sub_id)
+	form=SubjectForm(instance=sub)
+	if request.method=='POST':
+		form=SubjectForm(request.POST, instance=sub)
+		if form.is_valid():
+			form.save()
+			return redirect('create_subject')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_subject.html', context)
+def delete_subject(request, sub_id):
+	sub=Subject.objects.get(id=sub_id)
+	sub.delete()
+	return redirect('create_subject')
+# Edit registered class using  Registered Class Model
+def edit_regclass(request, cl_id):
+	cl=RegisteredClass.objects.get(id=cl_id)
+	form=RegisteredClassForm(instance=cl)
+	if request.method=='POST':
+		form=RegisteredClassForm(request.POST, instance=cl)
+		if form.is_valid():
+			form.save()
+			return redirect('create_registeredclass')
+	else:
+		context={'form':form}
+		return render(request, 'academics/edit_regcl.html', context)
+#Delete registered class using  Registered Class Model
+def delete_regclass(request, cl_id):
+	cl=RegisteredClass.objects.get(id=cl_id)
+	cl.delete()
+	return redirect('create_registeredclass')
