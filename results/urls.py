2021cf2c376f347bdee5fbec0666214aab7b1204
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2020-11-02 15:35:04 +0530

    Add files via upload

--- results/urls.py
+++ results/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-path('', views.result_list, name='result_list'),
-path('create_result/', views.create_result, name='create_result'),
-path('edit_result/<int:id>/', views.edit_result, name='edit_result'),
-path('delete_result/<int:id>/', views.delete_result, name='delete_result'),
+#Exam type urls
+path('types/', views.type_list, name='type_list'),
+path('create_type/', views.create_type, name='create_type'),
+path('edit_type/<int:id>/', views.edit_type, name='edit_type'),
+path('delete_type/<int:id>/', views.delete_type, name='delete_type'),
+
+#Exam urls
+path('', views.exam_list, name='exam_list'),
+path('create_exam/', views.create_exam, name='create_exam'),
+path('edit_exam/<int:id>/', views.edit_exam, name='exam'),
+path('delete_exam/<int:id>/', views.delete_exam, name='delete_exam'),
+
+#Exam agenda urls
+path('agenda/', views.agenda_list, name='agenda_list'),
+path('create_agenda/', views.create_agenda, name='create_agenda'),
+path('view_agenda/<int:id>/', views.view_agenda, name='view_agenda'),
+path('edit_agenda/<int:id>/', views.edit_agenda, name='edit_agenda'),
+path('delete_agenda/<int:id>/', views.delete_agenda, name='delete_agenda'),
+#Exam member urls
+path('examinees/', views.member_list, name='member_list'),
+path('create_member/', views.create_member, name='create_member'),
+path('view_member/<int:id>/', views.view_member, name='view_member'),
+path('edit_member/<int:id>/', views.edit_member, name='edit_member'),
+path('delete_member/<int:id>/', views.delete_member, name='delete_member'),
+
 ]
--- results/views.py
+++ results/views.py
@@ -1,33 +1,146 @@
-from django.shortcuts import render, redirect
-from results.models import Result
-from results.forms import ResultForm
+from django.shortcuts import render
+from .models import ExamType, Exam, ExamAgenda, ExamMember
+from .forms import ExamTypeForm, ExamForm, ExamAgendaForm, ExamMemberForm 
 # Create your views here.
-def result_list(request):
-	results=Result.objects.all()
-	context={'results':results}
-	return render(request, 'results/result_list.html', context)
-def create_result(request):
-	form=ResultForm()
+#Admin can view types of exams in a list
+def type_list(request):
+	types=ExamType.objects.all()
+	context={'types':types}
+	return render(request, 'exams/type_list.html', context)
+#Admin can create exam type
+def create_type(request):
+	form=ExamTypeForm()
 	if request.method=='POST':
-		form=ResultForm(request.POST)
+		form=ExamTypeForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect('result_list')
+			return redirect('type_list')
 	else:
 		context={'form':form}
-		return render(request, 'results/create_result.html', context)
-def edit_result(request, id):
-	result=Result.objects.get(id=id)
-	form=ResultForm(instance=result)
+		return render(request, 'exams/create_type.html', context)
+#Admin can edit exam type
+def edit_type(request, id):
+	exam_type=ExamType.objects.get(id=id)
+	form=ExamTypeForm(instance=exam_type)
 	if request.method=='POST':
-		form=ResultForm(request.POST, instance=result)
+		form=ExamTypeForm(request.POST, instance=exam_type)
 		if form.is_valid():
 			form.save()
-			return redirect('result_list')
+			return redirect('type_list')
 	else:
 		context={'form':form}
-		return render(request, 'results/edit_result.html', context)
-def delete_result(request, id):
-	result=Result.objects.get(id=id)
-	result.delete()
-	return redirect('result_list')
+		return render(request, 'exams/edit_type.html', context)
+#Admin can delete exam type
+def delete_type(request, id):
+	exam_type=ExamType.objects.get(id=id)
+	exam_type.delete()
+	return redirect('type_list')
+#Admin can view exam list
+def exam_list(request):
+	exams=Exam.objects.all()
+	context={'exams':exams}
+	return render(request, 'exams/exam_list.html', context)
+#Admin can create exam
+def create_exam(request):
+	form=ExamForm()
+	if request.method=='POST':
+		form=ExamForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('exam_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/create_exam.html', context)
+#Admin can edit exam
+def edit_exam(request, id):
+	exam=Exam.objects.get(id=id)
+	form=ExamForm(instance=exam)
+	if request.method=='POST':
+		form=ExamForm(request.POST, instance=exam)
+		if form.is_valid():
+			form.save()
+			return redirect('exam_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/edit_exam.html', context)
+#Admin can delete exam
+def delete_exam(request, id):
+	exam=Exam.objects.get(id=id)
+	exam.delete()
+	return redirect('exam_list')
+#Admin can view exam agenda list
+def agenda_list(request):
+	agenda=ExamAgenda.objects.all()
+	context={'agenda':agenda}
+	return render(request, 'exams/agenda_list.html', context)
+#Admin can create event agend
+def create_agenda(request):
+	form=ExamAgendaForm()
+	if request.method=='POST':
+		form=ExamAgendaForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('agenda_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/create_agenda.html', context)
+#admin can view agenda detail
+def view_agenda(request, id):
+	agenda=ExamAgenda.objects.get(id=id)
+	context={'agenda':agenda}
+	return render(request, 'exams/view_agenda.html', context)
+#Admin can edit agenda
+def edit_agenda(request, id):
+	agenda=ExamAgenda.objects.get(id=id)
+	form=ExamAgendaForm(instance=agenda)
+	if request.method=='POST':
+		form=ExamAgendaForm(request.POST, instance=agenda)
+		if form.is_valid():
+			form.save()
+			return redirect('agenda_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/edit_agenda.html', context)
+#Admin can delete agenda
+def delete_agenda(request, id):
+	agenda=ExamAgenda.objects.get(id=id)
+	agenda.delete()
+	return redirect('agenda_list')
+#Admin can view examinees/exam-member list
+def member_list(request):
+	members=ExamMember.objects.all()
+	context={'members':members}
+	return render(request, 'exams/member_list.html', context)
+#Admin can add examinee
+def create_member(request):
+	form=ExamMemberForm()
+	if request.method=='POST':
+		form=ExamMemberForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('member_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/create_member.html', context)
+#Admin can view examinee details
+def view_member(request, id):
+	member=ExamMember.objects.get(id=id)
+	context={'member':member}
+	return render(request, 'exams/view_member.html', context)
+#Admin can edit examinee
+def edit_member(request, id):
+	member=ExamMember.objects.get(id=id)
+	form=ExamMemberForm(instance=member)
+	if request.method=='POST':
+		form=ExamMemberForm(request.POST, instance=member)
+		if form.is_valid():
+			form.save()
+			return redirect('member_list')
+	else:
+		context={'form':form}
+		return render(request, 'exams/edit_member.html', context)
+#Admin can delete examinee
+def delete_member(request, id):
+	member=ExamMember.objects.get(id=id)
+	member.delete()
+	return redirect('member_list')
